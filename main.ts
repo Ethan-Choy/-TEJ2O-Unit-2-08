c779eb30bf89591b70f4a23807d02e414a5c1b0b
Ethan-Choy <ethan.choy@stu.ocsb.ca>
2025-10-03 13:13:38 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,55 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ethan Choy
+ * Created on: Sep 2025
+ * This program turns on and off RGM LEDs
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function() {
+    // turn on red
+       pins.digitalWritePin(DigitalPin.P13, 1)
+
+    // turn on blue
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+
+    // turn on green
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+
+    // turn on purple
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P14, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+
+    // turn on cyan
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+
+    // turn on yellow
+        basic.pause(1000)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        pins.digitalWritePin(DigitalPin.P14, 1)
+
+    // turn on white
+       basic.pause(1000)
+       pins.digitalWritePin(DigitalPin.P14, 1)
+       pins.digitalWritePin(DigitalPin.P13, 1)
+       pins.digitalWritePin(DigitalPin.P15, 1)
+
+    // turn off RGM LED
+       basic.pause(1000)
+       pins.digitalWritePin(DigitalPin.P14, 0)
+       pins.digitalWritePin(DigitalPin.P13, 0)
+       pins.digitalWritePin(DigitalPin.P15, 0)
+
+})
